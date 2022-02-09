d310f462b056ccd55a93b8acbb7ce28e56640f8c
EvilJadeon <thejadeon@vk.com>
2022-02-10 00:17:30 +0300

    Add migration to add author name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_09_161601) do
+ActiveRecord::Schema.define(version: 2022_02_09_210117) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2022_02_09_161601) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
+    t.string "author"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
